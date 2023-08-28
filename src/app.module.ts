cc1539543b7f7753b41675005b5e306817b959a4
Igor Kroeber <igor.kroeber@gmail.com>
2023-08-28 18:36:29 -0300

    #4: created configuration module for app-wide access

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UserModule } from './user/user.module'
 import { PrismaModule } from './prisma/prisma.module'
+import { ConfigurationModule } from './configuration/configuration.module'
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule, ConfigurationModule],
 })
 export class AppModule {}
